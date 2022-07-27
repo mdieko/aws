cc66598e10b041efa66fb59e700084aadbd718f0
Martin Muñoz <dmunoz@invertironline.com>
2022-07-27 11:28:57 -0300

    subnets

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -4,3 +4,16 @@
 variable "vpc-1-cidr" {
   description = "IPv4 CIDR"
 }
+
+variable "subnets" {
+    default = {
+       subnet_private_eks_1a = {
+          cidr_block = "10.20.0.0/18"
+          availability_zone = "us-east-1a"
+       }
+       subnet_private_eks_1a = {
+          cidr_block = "10.20.0.0/18"
+          availability_zone = "us-east-1a"
+       }
+    }
+} 
