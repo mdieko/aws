b2d2d6c7b7d2329f423ae750b4bd026ded94245c
Martin Muñoz <dmunoz@invertironline.com>
2022-07-27 11:32:43 -0300

    subnets

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -9,3 +9,13 @@ resource "aws_vpc" "vpc-1" {
     Name  = "vpc-1"
   }
 }
+
+resource "aws_subnet" "subnet" {
+
+  for_each = var.subnets
+
+  availability_zone = each.value.availability_zone
+  vpc_id = aws_vpc.vpc-homework2.id
+  cidr_block = each.value.cidr_block
+  map_public_ip_on_launch = each.value.map_public_ip_on_launch
+}
